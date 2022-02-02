59ffa7266d4f269b8ef4e963051f1bcdb3c7bd5f
Evail <evail@protonmail.com>
2022-02-02 10:21:22 +0100

    desktop react added
    -

---
+++ docs/components/SEO/Meta.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Head from 'next/head';
+
+export default function Meta() {
+    return (
+        <Head>
+           /* Primary Meta Tags */
+            <title>Evail devsite</title>
+            <meta charSet="utf-8" />
+            <meta name="title" content="Evail devsite" />
+            <meta name="description"
+                content="Evail devsite Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="author" content="Evail devsite" />
+            <meta name="keywords"
+                content="Evail devsite" />
+            <meta name="robots" content="index, follow" />
+            <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta name="language" content="English" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="theme-color" content="#E95420" />
+
+            /* Search Engine */
+            <meta name="image" content="images/logos/fevicon.png" />
+            /* Schema.org for Google */
+            <meta itemProp="name" content="Evail devsite" />
+            <meta itemProp="description"
+                content="Evail devsite. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta itemProp="image" content="images/logos/fevicon.png" />
+
+            <link rel="icon" href="images/logos/fevicon.svg" />
+            <link rel="apple-touch-icon" href="images/logos/logo.png" />
+            <link rel="preload" href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" as="style" />
+            <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet"></link>
+        </Head>
+    )
+}
